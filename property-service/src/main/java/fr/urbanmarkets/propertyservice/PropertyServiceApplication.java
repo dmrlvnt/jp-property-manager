8d70d880aae9cf167d1c267286191d99fdf2da4b
Levent Demir <dmrlvnt@gmail.com>
2018-04-14 12:40:56 +0200

    Start class names changes

--- property-service/src/main/java/fr/urbanmarkets/propertyservice/PropertyServiceApplication.java
+++ property-service/src/main/java/fr/urbanmarkets/propertyservice/PropertyServiceApplication.java
@@ -3,19 +3,13 @@ package fr.urbanmarkets.propertyservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class Application {
+public class PropertyServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication.run(PropertyServiceApplication.class, args);
   }
 
-  @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
 }
